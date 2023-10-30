405cc20c0c266aa4c9d5443d6c937fe9de25f187
yashtote <Yashtote9999@gmail.com>
2023-10-30 09:28:59 +0000

    First Commit

---
+++ Controllers/DeptController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using EMS.Models;
+using System.ComponentModel.DataAnnotations;
+namespace EMS.Controllers;
+
+
+public class DeptController : Controller
+{
+    private readonly EmsDbContext context;
+
+    public DeptController(EmsDbContext _context)
+    {
+        context = _context;
+    }
+    public IActionResult List()
+    {
+        List<Department> data= null;
+        try{
+            data = context.Departments.ToList();
+            if(data.Count == 0) throw new Exception();
+        }
+        catch (System.Exception ex){
+            ViewBag.ErrorMessage="0 records present";
+            return View("Error");
+        }
+        var data = context.Departments.ToList();
+        return View(data);
+    }
+    public IActionResult Create(){
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Department department){
+        if(ModelState.IsValid)
+        {
+            context.Departments.Add(department);
+            context.SaveChanges();
+            return RedirectToAction("List");
+        }
+        return View();
+    }
+
+}
---
+++ Controllers/EmpController.cs
@@ -0,0 +1,13 @@
+// // using System.Diagnostics;
+// using Microsoft.AspNetCore.Mvc;
+// // using EMS.Models;
+
+
+// namespace EMS.Controllers;
+
+
+// public class EmpController : Controller{
+//     public IActionResult List(){
+//     return View();
+// }
+// }
